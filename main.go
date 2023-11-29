be5cf08119a86120a063e2b1a828c055686822ef
Brian Hall <brian.hall4@gmail.com>
2023-11-28 22:47:48 -0500

    basic prompt print

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-  "fmt"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func main() {
-  fmt.Println("Hello, World!")
+	// Need to do an infinite for loop that prints a prompt
+	// and accepts commands
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Pokedex CLI")
+
+	for {
+		fmt.Print("pokedexcli> ")
+		text, _ := reader.ReadString('\n')
+		fmt.Println(text)
+	}
 }
